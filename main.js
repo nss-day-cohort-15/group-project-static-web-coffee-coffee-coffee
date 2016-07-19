8356e780148ef3de5dcae4d9aa576b853e656bd6
Fletcher Watson <fbwthe6@gmail.com>
2016-07-19 08:59:21 -0500

    filled out products array, created for loop.

--- main.js
+++ main.js
@@ -1,102 +1,69 @@
+
 var productsArray = [
   {
-    name:"smallMug",
-    description:"",
-    price: 0,
+    name:"Small Mug",
+    description:"Blue mountain, qui cup single origin extraction white robust variety, espresso spoon, and coffee as in plunger pot ut grinder. Viennese single shot and, doppio, wings cortado plunger pot et single origin, eu as viennese saucer as cortado. Pumpkin spice aromatic, robust, brewed doppio, foam instant dark latte macchiato affogato dark mug skinny. Ut acerbic a qui, ut to go cappuccino extra, frappuccino filter, cream brewed extra cup coffee, aged chicory aftertaste single shot cup body.",
+    price: "$5",
+    imageURL:"http://www.manrepeller.com/wp-content/uploads/2013/07/coffee-cup-01.jpg"
+  },
+
+  {
+    name:"Med Mug",
+    description:"Blue mountain, qui cup single origin extraction white robust variety, espresso spoon, and coffee as in plunger pot ut grinder. Viennese single shot and, doppio, wings cortado plunger pot et single origin, eu as viennese saucer as cortado. Pumpkin spice aromatic, robust, brewed doppio, foam instant dark latte macchiato affogato dark mug skinny. Ut acerbic a qui, ut to go cappuccino extra, frappuccino filter, cream brewed extra cup coffee, aged chicory aftertaste single shot cup body.",
+    price: "$6",
     imageURL:""
   },
+
   {
-    name:"medMug",
-    description:"",
-    price: 0,
+    name:"Large Mug",
+    description:"Blue mountain, qui cup single origin extraction white robust variety, espresso spoon, and coffee as in plunger pot ut grinder. Viennese single shot and, doppio, wings cortado plunger pot et single origin, eu as viennese saucer as cortado. Pumpkin spice aromatic, robust, brewed doppio, foam instant dark latte macchiato affogato dark mug skinny. Ut acerbic a qui, ut to go cappuccino extra, frappuccino filter, cream brewed extra cup coffee, aged chicory aftertaste single shot cup body.",
+    price: "$7",
     imageURL:""
   },
+
   {
-    name:"largeMug",
-    description:"",
-    price: 0,
+    name:"Travel Mug",
+    description:"Blue mountain, qui cup single origin extraction white robust variety, espresso spoon, and coffee as in plunger pot ut grinder. Viennese single shot and, doppio, wings cortado plunger pot et single origin, eu as viennese saucer as cortado. Pumpkin spice aromatic, robust, brewed doppio, foam instant dark latte macchiato affogato dark mug skinny. Ut acerbic a qui, ut to go cappuccino extra, frappuccino filter, cream brewed extra cup coffee, aged chicory aftertaste single shot cup body.",
+    price: "$8",
     imageURL:""
   },
+
   {
-    name:"travelMug",
-    description:"",
-    price: 0,
+    name:"Sippy Cup",
+    description:"Blue mountain, qui cup single origin extraction white robust variety, espresso spoon, and coffee as in plunger pot ut grinder. Viennese single shot and, doppio, wings cortado plunger pot et single origin, eu as viennese saucer as cortado. Pumpkin spice aromatic, robust, brewed doppio, foam instant dark latte macchiato affogato dark mug skinny. Ut acerbic a qui, ut to go cappuccino extra, frappuccino filter, cream brewed extra cup coffee, aged chicory aftertaste single shot cup body.",
+    price: "$3",
     imageURL:""
   },
+
   {
-    name:"sippyCupMug",
-    description:"",
-    price: 0,
-    imageURL:""
-  },
-  {
-    name:"yetiMug",
-    description:"",
-    price: 0,
+    name:"Yeti Mug",
+    description:"Blue mountain, qui cup single origin extraction white robust variety, espresso spoon, and coffee as in plunger pot ut grinder. Viennese single shot and, doppio, wings cortado plunger pot et single origin, eu as viennese saucer as cortado. Pumpkin spice aromatic, robust, brewed doppio, foam instant dark latte macchiato affogato dark mug skinny. Ut acerbic a qui, ut to go cappuccino extra, frappuccino filter, cream brewed extra cup coffee, aged chicory aftertaste single shot cup body.",
+    price: "$10",
     imageURL:""
   }
 ]
 
-var productsObj = {
-  name:"",
-  description:"",
-  price: 0,
-  imageURL:""
-}
-
-// for (var - i; i <= productsArray.length; i++) {
-
-// }
-
-console.log("hello");
-
-
-var currentoutput = `<article>
-    <div>
-      <h1>Small Mug</h1>
-    </div>
-  </article>
-
-  <article>
-    <div>
-      <h1>Medium Mug</h1>
-    </div>
-  </article>
-
-  <article>
-    <div>
-      <h1>Large Mug</h1>
-    </div>
-  </article>
-
-  <article>
-    <div>
-      <h1>Travel Mug</h1>
-    </div>
-  </article>
-
-  <article>
-    <div>
-      <h1>Sippy Mug</h1>
-    </div>
-  </article>
-
-  <article>
-    <div>
-      <h1>Yeti Mug</h1>
-    </div>
-  </article>
-
-  <article>
-    <div>
-      <h1>Dad Mug</h1>
-    </div>
-  </article>
-
-  <article>
-    <div>
-      <h1>Mug Mug</h1>
+for (var i = 0; i < productsArray.length; i++) {
+  var currentoutput = `<article>
+    <div class="card">
+      <h1>${productsArray[i].name}</h1>
+        <ul>
+          <li class="description">${productsArray[i].description}</li>
+          <li class="price">${productsArray[i].price}</li>
+          <li class="image"><img src=${productsArray[i].imageURL}></li>
+        </ul>
     </div>
   </article>`
 
-document.getElementById("output").innerHTML = currentoutput;
+  document.getElementById("output").innerHTML += currentoutput;
+
+  console.log(productsArray[i].name);
+
+}
+
+// console.log("hello");
+
+
+
+
+
